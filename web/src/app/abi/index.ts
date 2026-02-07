553b42c462d75c6ebfbb2c2fffcf5a25b055fe29
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-02-06 22:16:03 -0600

    added abi files in web project

---
+++ web/src/app/abi/index.ts
@@ -0,0 +1,6 @@
+export { ERC20_ABI } from "./ERC20";
+export { INVOICENFT_ABI } from "./InvoiceNFT";
+export { MOCKUSDC_ABI } from "./MockUSDC";
+export { TREASURY_ABI } from "./Treasury";
+export { VAULT_ABI } from "./Vault";
+export { VAULTFACTORY_ABI } from "./VaultFactory";
