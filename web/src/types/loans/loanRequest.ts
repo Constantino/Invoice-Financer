c3a817c6fb212a1b799b25a67ba21444f5d1eeed
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-02-07 00:22:01 -0600

    added loanRquest type

---
+++ web/src/types/loans/loanRequest.ts
@@ -0,0 +1,22 @@
+export interface LoanRequest {
+    id: number;
+    invoice_number: string;
+    invoice_amount: number;
+    invoice_due_date: string;
+    term: number;
+    customer_name: string;
+    monthly_interest_rate: number;
+    advance_rate: number;
+    max_loan: number;
+    delivery_completed: boolean;
+    assignment_signed: boolean;
+    not_pledged: boolean;
+    borrower_address: string;
+    invoice_file_url?: string;
+    token_id?: number;
+    token_uri?: string;
+    token_address?: string;
+    created_at: string;
+    modified_at: string;
+    status: string;
+}
