d622bfbcc62cc3138ad02f3b3f8a48f0f742d4cb
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-02-06 23:52:43 -0600

    added types for vault

---
+++ web/src/types/vault.ts
@@ -0,0 +1,40 @@
+export interface Vault {
+    vault_id: number;
+    vault_address: string;
+    borrower_address: string;
+    vault_name: string;
+    max_capacity: string;
+    current_capacity: string;
+    loan_request_id: number;
+    status: string;
+    funded_at: string | null;
+    fund_release_tx_hash: string | null;
+    created_at: string;
+    modified_at: string;
+}
+
+export interface LenderPortfolio {
+    lender_id: number;
+    lender_address: string;
+    amount: string;
+    tx_hash: string;
+    created_at: string;
+    vault_id: number;
+    vault_address: string;
+    vault_name: string;
+    borrower_address: string;
+    max_capacity: string;
+    current_capacity: string;
+    status: string;
+    funded_at: string | null;
+    fund_release_tx_hash: string | null;
+    fund_release_at?: string | null;
+    loan_request_id: number;
+    maturity_date?: string | null;
+    monthly_interest_rate?: number | null;
+    lender_status: "FUNDED" | "REDEEMED";
+    shares_amount?: string;
+    redeemed_amount?: string;
+    redemption_tx_hash?: string;
+    redeemed_at?: string;
+}
