7786eb53caa8b6e09452889c054b30145b756f9b
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-02-06 23:35:55 -0600

    added requestedCountStore

---
+++ web/src/stores/requestedCountStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface RequestedCountState {
+    requestedCount: number;
+    setRequestedCount: (count: number) => void;
+}
+
+export const useRequestedCountStore = create<RequestedCountState>()((set) => ({
+    requestedCount: 0,
+    setRequestedCount: (count) => set({ requestedCount: count }),
+}));
